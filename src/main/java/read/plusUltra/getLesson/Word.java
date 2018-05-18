b023d746cbfa108f597e0772148ec51450611ca7
Stephan <Stephan@here>
2018-05-19 01:20:54 +0200

    commit

--- src/main/java/read/plusUltra/getLesson/Word.java
+++ src/main/java/read/plusUltra/getLesson/Word.java
@@ -3,32 +3,18 @@ package read.plusUltra.getLesson;
 public class Word {
 	
 	private int id;
-	private String s1;
-	private String s2;
-	private String s3;
-	private String s4;
-	private String s5;
-	private String s6;
-	private String s7;
-	private String s8;
-	private String s9;
-	
+	private String text;
+	private String type;
+
 	public Word() {
 		
 	}
 
-	public Word(int id, String s1, String s2, String s3, String s4, String s5, String s6, String s7, String s8,
-			String s9) {
+	public Word(int id, String text, String type) {
+		super();
 		this.id = id;
-		this.s1 = s1;
-		this.s2 = s2;
-		this.s3 = s3;
-		this.s4 = s4;
-		this.s5 = s5;
-		this.s6 = s6;
-		this.s7 = s7;
-		this.s8 = s8;
-		this.s9 = s9;
+		this.text = text;
+		this.type = type;
 	}
 
 	public int getId() {
@@ -39,77 +25,21 @@ public class Word {
 		this.id = id;
 	}
 
-	public String getS1() {
-		return s1;
+	public String getText() {
+		return text;
 	}
 
-	public void setS1(String s1) {
-		this.s1 = s1;
+	public void setText(String text) {
+		this.text = text;
 	}
 
-	public String getS2() {
-		return s2;
+	public String getType() {
+		return type;
 	}
 
-	public void setS2(String s2) {
-		this.s2 = s2;
-	}
-
-	public String getS3() {
-		return s3;
-	}
-
-	public void setS3(String s3) {
-		this.s3 = s3;
-	}
-
-	public String getS4() {
-		return s4;
-	}
-
-	public void setS4(String s4) {
-		this.s4 = s4;
-	}
-
-	public String getS5() {
-		return s5;
-	}
-
-	public void setS5(String s5) {
-		this.s5 = s5;
-	}
-
-	public String getS6() {
-		return s6;
-	}
-
-	public void setS6(String s6) {
-		this.s6 = s6;
-	}
-
-	public String getS7() {
-		return s7;
-	}
-
-	public void setS7(String s7) {
-		this.s7 = s7;
-	}
-
-	public String getS8() {
-		return s8;
-	}
-
-	public void setS8(String s8) {
-		this.s8 = s8;
-	}
-
-	public String getS9() {
-		return s9;
-	}
-
-	public void setS9(String s9) {
-		this.s9 = s9;
+	public void setType(String type) {
+		this.type = type;
 	}
 	
-
+	
 }
