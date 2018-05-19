67fd72eaab6264bfc43f4e859c90a86b5387ca48
Stephan <Stephan@here>
2018-05-19 23:40:52 +0200

    .

--- src/main/java/read/plusUltra/sendStat/SendStats.java
+++ src/main/java/read/plusUltra/sendStat/SendStats.java
@@ -9,7 +9,6 @@ import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,7 +29,6 @@ public class SendStats {
 	@PostMapping("/send-stats")
 	public void sendStats(@RequestBody SendStatsRequest request) {
 		log.debug("send-stats");
-		
 		
 		SqlParameterSource in = new MapSqlParameterSource()
 				.addValue("I_ID", request.getId())
